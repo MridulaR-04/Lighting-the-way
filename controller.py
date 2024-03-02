c3e248e2969be39e4f990c9399e76be65402d4f0
Mridula Ranjit <rm4480414@gmail.com>
2024-03-02 15:51:23 +0530

    Added files

---
+++ controller.py
@@ -0,0 +1,50 @@
+import pyfirmata
+
+comport='COM5'
+
+board = pyfirmata.Arduino(comport)
+
+
+led_1=board.get_pin('d:7:o')
+led_2=board.get_pin('d:6:o')
+led_3=board.get_pin('d:5:o')
+led_4=board.get_pin('d:4:o')
+
+def led(fingerUp):
+    if fingerUp==[0,0,0,0,0]:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        
+
+    elif fingerUp==[0,1,0,0,0]:
+        led_1.write(1)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        
+    elif fingerUp==[0,1,1,0,0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(0)
+        led_4.write(0)
+            
+    elif fingerUp==[0,1,1,1,0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(0)
+        
+    elif fingerUp==[0,1,1,1,1]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        
+    elif fingerUp==[1,1,1,1,1]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        
